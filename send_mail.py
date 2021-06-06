af62553c985f40678140ee0bc823ad0dcda4ba67
Omar <3910032@upm.edu.sa>
2021-06-06 23:37:38 +0300

    improving mail sending process

--- send_mail.py
+++ send_mail.py
@@ -7,18 +7,17 @@ SENDER_EMAIL = environ['SENDER_EMAIL_ADDRESS']
 SENDER_PASS = environ['SENDER_EMAIL_PASSWORD']
 
 
-def send_confirmation_mail(recipient, link):
+def send_mail(recipient, subject, content):
     try:
-        mail_content = '''Please click the link below to confirm your email: {}'''.format(link)
 
         # Setup the MIME
         message = MIMEMultipart()
         message['From'] = SENDER_EMAIL
         message['To'] = recipient
-        message['Subject'] = 'UHMS Email confirmation'  # The subject line
+        message['Subject'] = subject  # The subject line
 
         # The body and the attachments for the mail
-        message.attach(MIMEText(mail_content, 'plain'))
+        message.attach(MIMEText(content, 'plain'))
         # Create SMTP session for sending the mail
 
         session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
@@ -32,30 +31,17 @@ def send_confirmation_mail(recipient, link):
         print('\n\n--------------------------------------------------------\n\n'
               + str(e) + '\n\n--------------------------------------------------------\n\n')
         return False
+
+
+def send_confirmation_mail(recipient, link):
+    subject = 'UHMS Email confirmation'
+    content = 'Please click the link below to confirm your email: {}'.format(link)
+
+    return send_mail(recipient, subject, content)
 
 
 def send_reset_mail(recipient, link):
-    try:
-        mail_content = '''Please click the link below to reset your password: {}'''.format(link)
+    subject = 'UHMS Password Reset'
+    content = 'Please click the link below to reset your password: {}'.format(link)
 
-        # Setup the MIME
-        message = MIMEMultipart()
-        message['From'] = SENDER_EMAIL
-        message['To'] = recipient
-        message['Subject'] = 'UHMS Password Reset'  # The subject line
-
-        # The body and the attachments for the mail
-        message.attach(MIMEText(mail_content, 'plain'))
-        # Create SMTP session for sending the mail
-
-        session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
-        session.starttls()  # enable security
-        session.login(SENDER_EMAIL, SENDER_PASS)  # login with mail_id and password
-        text = message.as_string()
-        session.sendmail(SENDER_EMAIL, recipient, text)
-        session.quit()
-        return True
-    except Exception as e:
-        print('\n\n--------------------------------------------------------\n\n'
-              + str(e) + '\n\n--------------------------------------------------------\n\n')
-        return False
+    return send_mail(recipient, subject, content)
